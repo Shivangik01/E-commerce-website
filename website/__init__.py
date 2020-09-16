0d8081da58c260f52edad76cccffeeae404ab492
Shivangik01 <shivangi.kochrekar@spit.ac.in>
2020-09-16 23:27:48 +0530

    login register

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template, request, Response, redirect, url_for
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from forms import RegistrationForm, LoginForm
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY']= 'ef7f9c363b428a622a04'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///site.db'
 db= SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+
+from website import routes
