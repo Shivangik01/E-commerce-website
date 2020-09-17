78b78768639750716403d1ada748362a655e2dc5
Rishita32 <rishitamote@gmail.com>
2020-09-18 03:16:32 +0530

    Index Page

--- website/models.py
+++ website/models.py
@@ -1,6 +1,8 @@
 from website import db,login_manager
 from flask_login import UserMixin
 
+
+
 @login_manager.user_loader
 def load_user(user_id):
 	return User.query.get(int(user_id))
@@ -18,16 +20,19 @@ class User(db.Model, UserMixin):
 
 
 
-#class Item(db.Model):
-#	id=db.Column(db.Integer, primary_key=True)
-#	title=db.Column(db.String(20), unique=True, nullable=False)
-#	description=db.Column(db.String(1000), nullable=False)
-#	price=db.Column(db.Integer, nullable=False)
-#	size=db.Column(db.String(5), nullable=False)
-#	color=db.Column(db.String(10), nullable=False)
-#	status=db.Column(db.String(10), nullable=False)
+class Item(db.Model):
+	id=db.Column(db.Integer, primary_key=True)
+	title=db.Column(db.String(20), unique=True, nullable=False)
+	description=db.Column(db.String(1000), nullable=False)
+	price=db.Column(db.Integer, nullable=False)
+	size=db.Column(db.String(5), nullable=False)
+	color=db.Column(db.String(10), nullable=False)
+	number=db.Column(db.Integer, nullable=False)
+	category=db.Column(db.String(20), nullable=False)
+	subcategory=db.Column(db.String(20), nullable=False)
+	img=db.Column(db.String(20), unique=True,nullable=False)
 
-#	def __repr__(self):
-#		return f"User('{self.title}' , '{self.description}', '{self.price}')"
+	def __repr__(self):
+		return f"User('{self.title}' , '{self.description}', '{self.price}')"
 
 
